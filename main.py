1cf8f7aac039e5318a26e44fe77bb8e7f854da1b
Jacob Gil <jacobgil325@gmail.com>
2021-07-06 16:50:09 +0000

    filtered data printing

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 # imports
 import requests
+# import sqlalchemy, requests, os, matplotlib
+# from sqlalchemy import create_engine 
+# import pandas as pd
+# import matplotlib.pyplot as plt
 
 # Input function 
 def user_input():
-    pass
+    return input("Enter a genre: ")
 
-
+  
+def page_input():
+    return input("How many animes would you like displayed? ")
+  
 # Here we define our query as a multi-line string
 query = '''
-query ($id: Int, $page: Int, $perPage: Int, $genre: String) {
+query ($id: Int, $page: Int, $perPage: Int, $genre: String, $popularity: Int) {
     Page (page: $page, perPage: $perPage) {
         pageInfo {
             total
@@ -17,12 +24,13 @@ query ($id: Int, $page: Int, $perPage: Int, $genre: String) {
             hasNextPage
             perPage
         }
-        media (id: $id, genre: $genre) {
+        media (id: $id, genre: $genre, popularity: $popularity) {
             id
             title {
                 romaji
                 native
             }
+            popularity
         }
     }
 }
@@ -30,16 +38,24 @@ query ($id: Int, $page: Int, $perPage: Int, $genre: String) {
 
 # Define our query variables and values that will be used in the query request
 variables = {
-    'genre': 'Fantasy',
+    'genre': user_input(),
     'page': 1,
-    'perPage': 5
+    'perPage': page_input()
 }
 
 url = 'https://graphql.anilist.co'
 
-# Make the HTTP Api request
-response = requests.post(url, json={'query': query, 'variables': variables})
-print(response.json())
+def handle_response():
+    # Make the HTTP Api request
+    response = requests.post(url, json={'query': query, 'variables': variables})
+    # testing filtering
+    response = response.json()
+    response = response['data']
+    response = response['Page']
+    response = response['media']
+    return response
+
+print(handle_response())
 
 # https://anilist.gitbook.io/anilist-apiv2-docs/overview/graphql/pagination
 
