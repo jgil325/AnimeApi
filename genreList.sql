c91fcec494688b229fe9287bf733e9bf6a9df7fe
Jacob Gil <59539299+jgil325@users.noreply.github.com>
2021-07-06 17:01:16 -0400

    Database utility added (#1)
    * testing database creation
    
    * database working - only second column correct
    
    * database working fully - does not check for existing or updates
    
    * names shortened and database fully working
    
    * dumped data into sql file for reuse
    
    * dumped data into sql file for reuse

---
+++ genreList.sql
@@ -0,0 +1,50 @@
+-- MariaDB dump 10.17  Distrib 10.4.8-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: genreList
+-- ------------------------------------------------------
+-- Server version	10.4.8-MariaDB-1:10.4.8+maria~bionic-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `genreList`
+--
+
+DROP TABLE IF EXISTS `genreList`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `genreList` (
+  `Anime Name` text DEFAULT NULL,
+  `Popularity` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `genreList`
+--
+
+LOCK TABLES `genreList` WRITE;
+/*!40000 ALTER TABLE `genreList` DISABLE KEYS */;
+INSERT INTO `genreList` VALUES ('Cowboy Bebop',202186),('Cowboy Bebop: Tengoku no Tobira',39228);
+/*!40000 ALTER TABLE `genreList` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-06 20:52:37
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
-import matplotlib.pyplot as plt
-import numpy as np
+# import matplotlib
+# import matplotlib.pyplot as plt
+import pandas as pd
+from sqlalchemy import create_engine
+import os
 
 # Retreives the genre based on the user's input
 def genre_input():
@@ -8,12 +11,12 @@ def genre_input():
 
 
 # Retreives the number of anime shows based on the user's input
-def page_input(): 
+def page_input():
     return input("How many animes would you like displayed? ")
-  
+
 
 # Defines the query as a multi-line string to be used with GraphQL
-def make_query():  
+def make_query():
     query = '''
     query ($id: Int, $page: Int, $perPage: Int, $genre: String, $popularity: Int) {
         Page (page: $page, perPage: $perPage) {
@@ -51,31 +54,59 @@ def make_variables():
 def handle_response(query, variables):
     url = 'https://graphql.anilist.co'
     # Make the HTTP Api request
-    response = requests.post(url, json={'query': query, 'variables': variables})
+    response = requests.post(
+        url, json={'query': query, 'variables': variables})
     # testing filtering
     response = response.json()
     response = response['data']
     response = response['Page']
     response = response['media']
     return response
-  
-  
+
+
 # Organizes the data into two columns, name and popularity
-def make_colummns(data):
-    dataFirstCol = []
-    for i in data:
-      dataFirstCol.append(i['title'])
-#     df = create_dataframe(dataFirstCol)
+def make_colummns(df, data):
     dataSecondCol = []
     for i in data:
-      dataSecondCol.append(i['popularity'])
-#     df.insert(1, "Popularity", dataSecondCol, True)
-#     return df
-    print('---------------------------------------')
-    print(dataFirstCol)
-    print('---------------------------------------')
-    print(dataSecondCol)
+        dataSecondCol.append(i['popularity'])
+    df.insert(1, "Popularity", dataSecondCol, True)
+    return df
+    # print('---------------------------------------')
+    # print(dataFirstCol)
+    # print('---------------------------------------')
+    # print(dataSecondCol)
+
+
+def create_dataframe(data):
+    # test dataframe creation
+    col_names = ['Anime Name']
+    dataFirstCol = []
+    for i in data:
+        string = str(i['title'])
+        dataFirstCol.append(string[12:-2])
+    df = pd.DataFrame(dataFirstCol, columns=col_names)
+    df = make_colummns(df, data)
+    return df
+
+
+def check_existing():
+    # create the database
+    os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS ' +
+              'genreList' + '; "')
+    # load
+    os.system("mysql -u root -pcodio genreList < genreList.sql")
+    df = pd.read_sql_table('genreList', con=create_engine('mysql:' +
+                                                           '//root:co' +
+                                                           'dio@loc' +
+                                                           'alhost/' +
+                                                           'genreList'))
+
     
+def data_to_sql(df):
+    # test uploading the dataframe to SQL
+    engine = create_engine('mysql://root:codio@localhost/genreList')
+    df.to_sql('genreList', con=engine, if_exists='replace', index=False)
+
 
 # Runs the program
 def main():
@@ -83,18 +114,16 @@ def main():
     query = make_query()
     data = handle_response(query, variables)
     print(data)
-    make_colummns(data)
-    
-    
-if __name__ == "__main__":
-    main() 
-    
-    
-# DOCUMENTATION
+    check_existing()
+    df = create_dataframe(data)
+    data_to_sql(df)
+    os.system("mysqldump -u root -pcodio genreList > genreList.sql")
 
-# -------------
-# testing git mob with new commit
-# -------------
+
+if __name__ == "__main__":
+    main()
+
+# DOCUMENTATION
 
 # https://anilist.gitbook.io/anilist-apiv2-docs/overview/graphql/pagination
 
